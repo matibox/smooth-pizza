070ba188b10826768669231f4ae0dfc2822d88f3
Matibox <mateusz.hladky@gmail.com>
2023-01-19 14:43:24 +0100

    Add no user protection to AddToCart component

--- client/src/components/AddToCart.tsx
+++ client/src/components/AddToCart.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import {
   type Dispatch,
   type SetStateAction,
@@ -5,6 +6,7 @@ import {
   useState,
   useMemo,
 } from 'react';
+import { useAuth } from '../context/AuthContext';
 import type { Product } from '../types/Product';
 import ProductEl from './Product';
 
@@ -14,6 +16,7 @@ type AddToCartProps = {
 };
 
 const AddToCart: FC<AddToCartProps> = ({ product, setProduct }) => {
+  const { user } = useAuth();
   const [count, setCount] = useState(1);
 
   const totalPrice = useMemo(() => {
@@ -33,34 +36,49 @@ const AddToCart: FC<AddToCartProps> = ({ product, setProduct }) => {
         >
           X
         </button>
-        <h2 className='w-full text-center text-2xl text-stone-900'>
-          Add to cart
-        </h2>
-        <div className='flex w-full items-center justify-between'>
-          <div>
-            <ProductEl product={product} addToCart={false} />
-          </div>
-          <div className='flex w-32 justify-between bg-amber-500 py-2 px-4 ring-1 ring-stone-50'>
-            <button
-              onClick={() =>
-                setCount(prev => {
-                  if (prev === 1) return prev;
-                  return prev - 1;
-                })
-              }
+        {user ? (
+          <>
+            <h2 className='w-full text-center text-2xl text-stone-900'>
+              Add to cart
+            </h2>
+            <div className='flex w-full items-center justify-between'>
+              <div>
+                <ProductEl product={product} addToCart={false} />
+              </div>
+              <div className='flex w-32 justify-between bg-amber-500 py-2 px-4 ring-1 ring-stone-50'>
+                <button
+                  onClick={() =>
+                    setCount(prev => {
+                      if (prev === 1) return prev;
+                      return prev - 1;
+                    })
+                  }
+                >
+                  -
+                </button>
+                <span className='font-bold'>{count}</span>
+                <button onClick={() => setCount(prev => prev + 1)}>+</button>
+              </div>
+            </div>
+            <div className='flex w-full items-center justify-between self-end sm:w-max sm:gap-8'>
+              <span className='text-lg font-bold'>Total: {totalPrice} €</span>
+              <button className='w-32 px-4 py-1 ring-1 ring-stone-900 transition-all hover:text-amber-600 hover:ring-amber-600 focus-visible:text-amber-600 focus-visible:outline-none focus-visible:ring-amber-600'>
+                Add to cart
+              </button>
+            </div>
+          </>
+        ) : (
+          <span>
+            Please{' '}
+            <Link
+              href='/signin'
+              className='text-amber-600 hover:text-amber-500'
             >
-              -
-            </button>
-            <span className='font-bold'>{count}</span>
-            <button onClick={() => setCount(prev => prev + 1)}>+</button>
-          </div>
-        </div>
-        <div className='flex w-full items-center justify-between self-end sm:w-max sm:gap-8'>
-          <span className='text-lg font-bold'>Total: {totalPrice} €</span>
-          <button className='w-32 px-4 py-1 ring-1 ring-stone-900 transition-all hover:text-amber-600 hover:ring-amber-600 focus-visible:text-amber-600 focus-visible:outline-none focus-visible:ring-amber-600'>
-            Add to cart
-          </button>
-        </div>
+              sign in
+            </Link>{' '}
+            to order online.
+          </span>
+        )}
       </div>
     </>
   );
